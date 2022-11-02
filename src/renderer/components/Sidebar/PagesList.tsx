ecf94550f036c080d3d36d96bff137c7bed1c191
Albert Mazur <kontakt@mazuralbert.pl>
2022-11-02 11:36:28 +0100

    added tailwindcss

---
+++ src/renderer/components/Sidebar/PagesList.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
+interface PagesListPros {
+  data: {
+    name: string;
+    href: string;
+  }[];
+}
+
+export const PagesList = ({ data }: PagesListPros) => {
+  return (
+    <ul>
+      {data.map(({ name, href }) => (
+        <li className="flex ">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? 'bg-teal-700 p-1.5 mx-1.5 mt-1.5 w-full rounded-lg text-white'
+                : 'p-1.5 mx-1.5 mt-1.5 w-full rounded-lg text-black'
+            }
+            to={href}
+          >
+            {name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
